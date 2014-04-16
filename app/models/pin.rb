86dc1777b63e2b430f30ce670bb27efc7f2be738
Ryan Brown <funkpocket@hotmail.com>
2014-04-16 13:34:22 -0400

    Add image upload with Paperclip

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,5 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
+
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => 'http://www.clker.com/cliparts/p/t/5/u/a/b/sherriff-hi.png'
 end
